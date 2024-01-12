a1c3c5804ee225a87607cba4e06e0476122ef9b2
ssili126 <112532343+ssili126@users.noreply.github.com>
2024-01-12 14:21:51 +0800

    Update IPTV-hebei.py

--- IPTV-hebei.py
+++ IPTV-hebei.py
@@ -7,8 +7,13 @@ import re
 url = "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22%E6%B2%B3%E5%8C%97%22"
 #url = "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iSGViZWki"
 
-# 使用webdriver_manager自动下载和管理chromedriver
-driver = webdriver.Chrome()
+# 创建一个Chrome WebDriver实例
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=chrome_options)
 
 # 使用WebDriver访问网页
 driver.get(url)  # 将网址替换为你要访问的网页地址
