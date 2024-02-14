faa738e486c787ad7abb380ca277cf6a7c25406d
ssili126 <112532343+ssili126@users.noreply.github.com>
2024-02-14 08:42:10 +0800

    更新 itvtest.py

--- itvtest.py
+++ itvtest.py
@@ -208,4 +208,5 @@ with open("itvlist.m3u", 'w', encoding='utf-8') as file:
                 file.write(f"#EXTINF:-1 group-title=\"其他频道\",{channel_name}\n")
                 file.write(f"{channel_url}\n")
                 channel_counters[channel_name] = 1
-
+    
+    file.write(f"#EXTINF:-1 group-title=\"{now_today}更新\"\n")
