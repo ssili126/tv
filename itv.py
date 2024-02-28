9bc1bafc6baa566fb5d7541546a2a71a4902e247
ssili126 <112532343+ssili126@users.noreply.github.com>
2024-02-29 07:20:06 +0800

    更新 itv.py

--- itv.py
+++ itv.py
@@ -74,142 +74,143 @@ def is_url_accessible(url):
 results = []
 
 for url in urls:
-    # 创建一个Chrome WebDriver实例
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-
-    driver = webdriver.Chrome(options=chrome_options)
-    # 使用WebDriver访问网页
-    driver.get(url)  # 将网址替换为你要访问的网页地址
-    time.sleep(10)
-    # 获取网页内容
-    page_content = driver.page_source
-
-    # 关闭WebDriver
-    driver.quit()
-
-    # 查找所有符合指定格式的网址
-    pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
-    urls_all = re.findall(pattern, page_content)
-    # urls = list(set(urls_all))  # 去重得到唯一的URL列表
-    urls = set(urls_all)  # 去重得到唯一的URL列表
-    x_urls = []
-    for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
-        url = url.strip()
-        ip_start_index = url.find("//") + 2
-        ip_end_index = url.find(":", ip_start_index)
-        ip_dot_start = url.find(".") + 1
-        ip_dot_second = url.find(".", ip_dot_start) + 1
-        ip_dot_three = url.find(".", ip_dot_second) + 1
-        base_url = url[:ip_start_index]  # http:// or https://
-        ip_address = url[ip_start_index:ip_dot_three]
-        port = url[ip_end_index:]
-        ip_end = "1"
-        modified_ip = f"{ip_address}{ip_end}"
-        x_url = f"{base_url}{modified_ip}{port}"
-        x_urls.append(x_url)
-    urls = set(x_urls)  # 去重得到唯一的URL列表
-
-    valid_urls = []
-    #   多线程获取可用url
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-        futures = []
-        for url in urls:
+    try:
+        # 创建一个Chrome WebDriver实例
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+    
+        driver = webdriver.Chrome(options=chrome_options)
+        # 使用WebDriver访问网页
+        driver.get(url,timeout=10)  # 将网址替换为你要访问的网页地址
+        time.sleep(10)
+        # 获取网页内容
+        page_content = driver.page_source
+    
+        # 关闭WebDriver
+        driver.quit()
+    
+        # 查找所有符合指定格式的网址
+        pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
+        urls_all = re.findall(pattern, page_content)
+        # urls = list(set(urls_all))  # 去重得到唯一的URL列表
+        urls = set(urls_all)  # 去重得到唯一的URL列表
+        x_urls = []
+        for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
             url = url.strip()
-            modified_urls = modify_urls(url)
-            for modified_url in modified_urls:
-                futures.append(executor.submit(is_url_accessible, modified_url))
-
-        for future in concurrent.futures.as_completed(futures):
-            result = future.result()
-            if result:
-                valid_urls.append(result)
-
-    for url in valid_urls:
-        print(url)
-    # 遍历网址列表，获取JSON文件并解析
-    for url in valid_urls:
-        try:
-            # 发送GET请求获取JSON文件，设置超时时间为0.5秒
             ip_start_index = url.find("//") + 2
+            ip_end_index = url.find(":", ip_start_index)
             ip_dot_start = url.find(".") + 1
-            ip_index_second = url.find("/", ip_dot_start)
+            ip_dot_second = url.find(".", ip_dot_start) + 1
+            ip_dot_three = url.find(".", ip_dot_second) + 1
             base_url = url[:ip_start_index]  # http:// or https://
-            ip_address = url[ip_start_index:ip_index_second]
-            url_x = f"{base_url}{ip_address}"
-
-            json_url = f"{url}"
-            response = requests.get(json_url, timeout=0.5)
-            json_data = response.json()
-
+            ip_address = url[ip_start_index:ip_dot_three]
+            port = url[ip_end_index:]
+            ip_end = "1"
+            modified_ip = f"{ip_address}{ip_end}"
+            x_url = f"{base_url}{modified_ip}{port}"
+            x_urls.append(x_url)
+        urls = set(x_urls)  # 去重得到唯一的URL列表
+    
+        valid_urls = []
+        #   多线程获取可用url
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            futures = []
+            for url in urls:
+                url = url.strip()
+                modified_urls = modify_urls(url)
+                for modified_url in modified_urls:
+                    futures.append(executor.submit(is_url_accessible, modified_url))
+    
+            for future in concurrent.futures.as_completed(futures):
+                result = future.result()
+                if result:
+                    valid_urls.append(result)
+    
+        for url in valid_urls:
+            print(url)
+        # 遍历网址列表，获取JSON文件并解析
+        for url in valid_urls:
             try:
-                # 解析JSON文件，获取name和url字段
-                for item in json_data['data']:
-                    if isinstance(item, dict):
-                        name = item.get('name')
-                        urlx = item.get('url')
-                        if ',' in urlx:
-                            urlx=f"aaaaaaaa"
-                            
-                        #if 'http' in urlx or 'udp' in urlx or 'rtp' in urlx:
-                        if 'http' in urlx:
-                            urld = f"{urlx}"
-                        else:
-                            urld = f"{url_x}{urlx}"
-
-                        if name and urld:
-                            # 删除特定文字
-                            name = name.replace("cctv", "CCTV")
-                            name = name.replace("中央", "CCTV")
-                            name = name.replace("央视", "CCTV")
-                            name = name.replace("高清", "")
-                            name = name.replace("超高", "")
-                            name = name.replace("HD", "")
-                            name = name.replace("标清", "")
-                            name = name.replace("频道", "")
-                            name = name.replace("-", "")
-                            name = name.replace(" ", "")
-                            name = name.replace("PLUS", "+")
-                            name = name.replace("＋", "+")
-                            name = name.replace("(", "")
-                            name = name.replace(")", "")
-                            name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
-                            name = name.replace("CCTV1综合", "CCTV1")
-                            name = name.replace("CCTV2财经", "CCTV2")
-                            name = name.replace("CCTV3综艺", "CCTV3")
-                            name = name.replace("CCTV4国际", "CCTV4")
-                            name = name.replace("CCTV4中文国际", "CCTV4")
-                            name = name.replace("CCTV4欧洲", "CCTV4")
-                            name = name.replace("CCTV5体育", "CCTV5")
-                            name = name.replace("CCTV6电影", "CCTV6")
-                            name = name.replace("CCTV7军事", "CCTV7")
-                            name = name.replace("CCTV7军农", "CCTV7")
-                            name = name.replace("CCTV7农业", "CCTV7")
-                            name = name.replace("CCTV7国防军事", "CCTV7")
-                            name = name.replace("CCTV8电视剧", "CCTV8")
-                            name = name.replace("CCTV9记录", "CCTV9")
-                            name = name.replace("CCTV9纪录", "CCTV9")
-                            name = name.replace("CCTV10科教", "CCTV10")
-                            name = name.replace("CCTV11戏曲", "CCTV11")
-                            name = name.replace("CCTV12社会与法", "CCTV12")
-                            name = name.replace("CCTV13新闻", "CCTV13")
-                            name = name.replace("CCTV新闻", "CCTV13")
-                            name = name.replace("CCTV14少儿", "CCTV14")
-                            name = name.replace("CCTV15音乐", "CCTV15")
-                            name = name.replace("CCTV16奥林匹克", "CCTV16")
-                            name = name.replace("CCTV17农业农村", "CCTV17")
-                            name = name.replace("CCTV17农业", "CCTV17")
-                            name = name.replace("CCTV5+体育赛视", "CCTV5+")
-                            name = name.replace("CCTV5+体育赛事", "CCTV5+")
-                            name = name.replace("CCTV5+体育", "CCTV5+")
-                            if 'udp' not in urld or 'rtp' not in urld:
-                                results.append(f"{name},{urld}")
+                # 发送GET请求获取JSON文件，设置超时时间为0.5秒
+                ip_start_index = url.find("//") + 2
+                ip_dot_start = url.find(".") + 1
+                ip_index_second = url.find("/", ip_dot_start)
+                base_url = url[:ip_start_index]  # http:// or https://
+                ip_address = url[ip_start_index:ip_index_second]
+                url_x = f"{base_url}{ip_address}"
+    
+                json_url = f"{url}"
+                response = requests.get(json_url, timeout=0.5)
+                json_data = response.json()
+    
+                try:
+                    # 解析JSON文件，获取name和url字段
+                    for item in json_data['data']:
+                        if isinstance(item, dict):
+                            name = item.get('name')
+                            urlx = item.get('url')
+                            if ',' in urlx:
+                                urlx=f"aaaaaaaa"
+                                
+                            #if 'http' in urlx or 'udp' in urlx or 'rtp' in urlx:
+                            if 'http' in urlx:
+                                urld = f"{urlx}"
+                            else:
+                                urld = f"{url_x}{urlx}"
+    
+                            if name and urld:
+                                # 删除特定文字
+                                name = name.replace("cctv", "CCTV")
+                                name = name.replace("中央", "CCTV")
+                                name = name.replace("央视", "CCTV")
+                                name = name.replace("高清", "")
+                                name = name.replace("超高", "")
+                                name = name.replace("HD", "")
+                                name = name.replace("标清", "")
+                                name = name.replace("频道", "")
+                                name = name.replace("-", "")
+                                name = name.replace(" ", "")
+                                name = name.replace("PLUS", "+")
+                                name = name.replace("＋", "+")
+                                name = name.replace("(", "")
+                                name = name.replace(")", "")
+                                name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
+                                name = name.replace("CCTV1综合", "CCTV1")
+                                name = name.replace("CCTV2财经", "CCTV2")
+                                name = name.replace("CCTV3综艺", "CCTV3")
+                                name = name.replace("CCTV4国际", "CCTV4")
+                                name = name.replace("CCTV4中文国际", "CCTV4")
+                                name = name.replace("CCTV4欧洲", "CCTV4")
+                                name = name.replace("CCTV5体育", "CCTV5")
+                                name = name.replace("CCTV6电影", "CCTV6")
+                                name = name.replace("CCTV7军事", "CCTV7")
+                                name = name.replace("CCTV7军农", "CCTV7")
+                                name = name.replace("CCTV7农业", "CCTV7")
+                                name = name.replace("CCTV7国防军事", "CCTV7")
+                                name = name.replace("CCTV8电视剧", "CCTV8")
+                                name = name.replace("CCTV9记录", "CCTV9")
+                                name = name.replace("CCTV9纪录", "CCTV9")
+                                name = name.replace("CCTV10科教", "CCTV10")
+                                name = name.replace("CCTV11戏曲", "CCTV11")
+                                name = name.replace("CCTV12社会与法", "CCTV12")
+                                name = name.replace("CCTV13新闻", "CCTV13")
+                                name = name.replace("CCTV新闻", "CCTV13")
+                                name = name.replace("CCTV14少儿", "CCTV14")
+                                name = name.replace("CCTV15音乐", "CCTV15")
+                                name = name.replace("CCTV16奥林匹克", "CCTV16")
+                                name = name.replace("CCTV17农业农村", "CCTV17")
+                                name = name.replace("CCTV17农业", "CCTV17")
+                                name = name.replace("CCTV5+体育赛视", "CCTV5+")
+                                name = name.replace("CCTV5+体育赛事", "CCTV5+")
+                                name = name.replace("CCTV5+体育", "CCTV5+")
+                                if 'udp' not in urld or 'rtp' not in urld:
+                                    results.append(f"{name},{urld}")
+                except:
+                    continue
             except:
                 continue
-        except:
-            continue
 
 
 results = set(results)  # 去重得到唯一的URL列表
